cde737b45a2dc5db25567f48ef16ede36f2028b4
mzntori <mzntori@proton.me>
2025-07-23 16:25:35 +0200

    a

--- flake.nix
+++ flake.nix
@@ -1,31 +1,37 @@
 {
   description = "order";
 
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./old
+      ];
+      systems = [ "x86_64-linux" ];
+      perSystem =
+        {
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            name = "order-dev";
+            packages = with pkgs; [
+              alejandra
+              git
+              helix
+            ];
+          };
+          formatter = pkgs.alejandra;
+        };
+    };
+
   inputs = {
     # nixpkgs
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
 
-  outputs =
-    {
-      self,
-      pkgs,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-    in
-    {
-      devShells.${system}.default = pkgs.mkShell {
-        name = "order-dev";
-        packages = with pkgs; [
-          git
-          helix
-          fish
-        ];
-        shellHook = ''
-          	fish
-        '';
-      };
-    };
+    # utils
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
 }
